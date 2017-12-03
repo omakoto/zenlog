ca08cd71c5c9f94db3b85084e1004d93321df44d
Makoto Onuki <omakoto@google.com>
2017-12-03 14:55:02 -0800

    E2E test for zsh

---
+++ e2etests/test05-zsh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+medir="${0%/*}"
+
+TEST_NAME=05
+export ZDOTDIR=./e2etests/files/zshrc
+export ZENLOG_START_COMMAND="exec /usr/bin/zsh -l"
+. "$medir/zenlog-test-common"
+
+clear_log
+
+cd "$medir"
+
+run_zenlog <<EOF
+cat data/fstab | grep -v -- '^#' #TAG
+echo $_ZENLOG_E2E_EXIT_TIME >"$_ZENLOG_TIME_INJECTION_FILE"; exit
+EOF
+
+check_result

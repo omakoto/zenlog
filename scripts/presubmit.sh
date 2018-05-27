3189068490c9273fccf85eb4062ac0308568d42c
Makoto Onuki <omakoto@google.com>
2018-05-26 22:39:31 -0700

    lint

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -7,16 +7,10 @@ set -e
 #go get github.com/golang/lint/golint                        # Linter
 #go get honnef.co/go/tools/cmd/megacheck                     # Badass static analyzer/linter
 
-NOT_FORMATTED=$(gofmt -s -d $(find . -type f -name '*.go'))
-
-if [[ -n "$NOT_FORMATTED" ]] ; then
-    echo $'Wrong format:\n'"$NOT_FORMATTED"
-    exit 1
-fi
+gofmt -s -d $(find . -type f -name '*.go') |& perl -pe 'END{exit($. > 0 ? 1 : 0)}'
 
 go test -v -race ./...                   # Run all the tests with the race detector enabled
+
 go vet ./...                             # go vet is the official Go static analyzer
 megacheck ./...                          # "go vet on steroids" + linter
-# golint -set_exit_status $(go list ./...) # one last linter
-
-./scripts/e2e-test.sh
+golint $(go list ./...) |& grep -v 'exported .* should have' | perl -pe 'END{exit($. > 0 ? 1 : 0)}'

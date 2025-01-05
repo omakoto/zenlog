d097394cef1ac8d48fc811025fd5fcc3a58be3fc
Makoto Onuki <makoto.onuki@gmail.com>
2025-01-04 23:32:55 -0800

    Clean up + test fix
    - Fixed tests
    - Removed docker and travis related files.

--- presubmit.sh
+++ presubmit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Extract commands from .travis.yml and execute them.
-
 set -e
 
 cd "${0%/*}"
@@ -11,6 +9,5 @@ run() {
   "$@"
 }
 
-. <(sed -ne 's/^ *- *\(.*\)#presubmit/run \1/p' .travis.yml)
-
-echo '(Run [docker rmi $(docker images -f "dangling=true" -q)] to remove dangling images if needed.)'
+./scripts/presubmit.sh
+./scripts/e2e-test.sh
